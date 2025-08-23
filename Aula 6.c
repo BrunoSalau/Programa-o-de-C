8a2129448d3669a9c7c9683955838fef1929d9d9
BrunoSalau <brunosalau@gmail.com>
2025-08-22 21:39:16 -0300

    Create Aula 6.c

---
+++ Aula 6.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+int main()
+{
+    /*double a,b,c,soma;
+    printf("digite um numero");
+    scanf ("%lf",&a);
+    printf ("Digite um outro numero que irei somar com o anterior");
+    scanf ("%lf",&b);
+    soma = a + b;
+    if(soma > 10){
+        printf("A soma e maior que 10");
+    }
+    else {
+        printf("A soma e menor que 10");
+    }
+
+    return 0;*/
+
+    /*double c;
+    printf("digite um numero");
+    scanf ("%lf",&c);
+    c = c++;
+    printf("o valor de C eh:%2.lf",c);
+    return 0;*/
+
+    /*int n;
+    printf ("Informe um nuemro");
+    scanf ("%d",&n);
+    if ( n > 0){
+        printf("\nO numero eh positivo\n");
+    }
+    else if(n == 0){
+        printf("\nO numero eh nulo\n");
+    }
+    else{
+        printf("\nO numero eh negativo\n");
+}
+    return 0;*/
+
+    /*int x, y;
+    printf("digite um numero x:");
+    scanf("%d",&x);
+    printf("digite um numero y:");
+    scanf("%d",&y);
+    x = x % 2;
+    y = y % 2;
+    if (x == 0 && y ==0){
+        printf("Os dois numero sao pares");
+    }
+    else if (x == 0 || y ==0){
+        printf("Um dos numeros e impar e o outro e par");
+    }
+    else {
+        printf("Os dois numeros sao impares");
+   }
+   return 0;*/
+
+   /*char sexo;
+   double altura, peso;
+   printf("Informe o sexo:");
+   scanf ("%c", &sexo);
+   printf ("Informe a altura:");
+   scanf("%lf", &altura);
+   if(sexo == 'm' || sexo == 'M'){
+    peso = (72.7 *altura) - 58;
+    printf("Peso ideal: %.2lf\n",peso);
+   }
+   else if(sexo == 'f' || sexo == 'F'){
+    peso = (62.1 * altura) - 44.7;
+    printf("Peso ideal: %.2lf\n", peso);
+   }
+   else {
+    printf("opcao invalida!");
+   }
+   return 0;*/
+
+
+
+   }
