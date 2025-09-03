fced2fc237001c38395471541a152202631916a5
BrunoSalau <brunosalau@gmail.com>
2025-09-03 13:48:08 -0300

    Ex 1.c

--- Trabalho 2/Ex 1.c
+++ Trabalho 2/Ex 1.c
@@ -5,22 +5,17 @@ int main()
 {
 int i,o;
     double a,b,c,media;
-    if(o == 0){
         printf("Por favor, digite um numero inteiro:");
         scanf("%d",&i);
         printf("\nPerfeito, agora de mais tres valores (pode ser inteiros ou decimais):");
         scanf("%lf %lf %lf",&a,&b,&c);
         o = i % 2;
+    if(o == 0){
         printf("\n\nValores dados: %.2lf, %.2lf, %.2lf\n\n",a,b,c);
         media = (a+b+c)/3;
         printf("\n--Ja que o valor de i eh par, a media aritimetica dos tres numeros anteriores:%.2lf\n\n",media);
     }
     else if(o == 1 && i>10){
-        printf("Por favor, digite um numero inteiro:");
-        scanf("%d",&i);
-        printf("\nPerfeito, agora de mais tres valores (pode ser inteiros ou decimais):");
-        scanf("%lf %lf %lf",&a,&b,&c);
-        o = i % 2;
         printf("\n\nValores dados: %.2lf, %.2lf, %.2lf\n\n",a,b,c);
         printf("\n\n--Pesos dos valores dados respectivamente:2, 3, 4.\n\n");
         media = ((a * 2)+(b * 3)+(c * 4))/9;
