7ca61c6a15154ec4619d0926e4a6e30bd1fa11c9
BrunoSalau <brunosalau@gmail.com>
2025-09-03 21:38:34 -0300

    Aula 8.c

---
+++ Aula 8.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main()
+{
+    int n,i=1;
+
+    printf("digite um numero:");
+    scanf("%d",&n);
+
+    if(n > 0){
+     while(i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+    }
+    else{
+        printf("numero negativo nao funciona!!");
+    }
+    return 0;
+}*/
+
+/*int main()
+{
+    int n,i=1;
+
+    printf("digite um numero:");
+    scanf("%d",&n);
+
+    while(n>=1){
+        printf("%d\n",n);
+        n--;
+    }
+
+    return 0;
+}*/
+/*int main()
+{
+    int n;
+
+    printf("digite um numero:");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}*/
+/*int main()
+{
+    int n,s=0,c=1;
+    while
+        (c<=5){
+        printf("Digite o %d numero:", c);
+        scanf("%d",&n);
+        s = s + n;
+        c++;
+    }
+    printf("A soma dos 5 numeros digitados eh:%d",s);
+    return 0;
+}*/
+/*int main()
+{
+    int n,s=0,c=1;
+    while
+        (c<=10){
+        printf("Digite o %d numero:", c);
+        scanf("%d",&n);
+        s = s + n;
+        c++;
+    }
+    s = s/10;
+    printf("A media aritimetica dos 10 numeros eh:%d",s);
+    return 0;
+}*/
+/*int main()
+{
+    int n,i=1,soma=0;
+    printf("Digite um valor para n:");
+    scanf("%d",&n);
+    while(i <= n){
+        soma = soma + i;
+        i++;
+    }
+    printf("Soma: %d",soma);
+    return 0;
+}*/
+/*int main()
+{
+    int n, qnt=0, soma=0;
+    printf("Qual numero voce quer somar na media aritimetica:");
+    scanf("%d",&n);
+    while(n != 0){
+        soma = soma + n;
+        qnt++;
+    printf("Digite outro numero voce quer somar na media aritimetica:");
+    scanf("%d",&n);
+    }
+    printf("A media aritimetica dos numeros que voce forneceu eh:%d",(soma/qnt));
+    return 0;
+}*/
