5ad7e6f5687ef2e71029ad1bc88ca61c72a6af72
BrunoSalau <brunosalau@gmail.com>
2025-09-05 21:53:03 -0300

    Aula 9.c

---
+++ Aula 9.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int num,maior,menor,indice = 1;
+    do{
+        printf("Informe um numero");
+        scanf("%d",&num);
+        if (indice == 1){
+            maior = num;
+            menor = num;
+            indice++;
+        }
+        else if(num != 0){
+            if(num > maior){
+                maior = num;}
+            else if(num < menor){
+                menor = num;
+            }
+        }
+
+    }while(num != 0);
+    printf("Maior:%d\n",maior);
+    printf("Menor:%d\n",menor);*/
+
+    /*for(int i = 1;i<=10;i++){
+        printf("%d\n",i);
+    }*/
+
+
+    /*for(int i=1;i<=20;i++){
+        printf("%d^2 = %d\n",i,(i*i));
+    }*/
+    /*int a;
+    printf("Me de um numero:");
+    scanf("%d",&a);
+    for(int i=0;i<=a;i++){
+        if(i%3 == 0){
+            printf("%d\n",i);
+        }
+        else{
+
+        }
+
+    }*/
+    /*int n;
+    printf("ME DE UM NUMERO!!!!!!!!!!!!:");
+    scanf("%d",&n);
+    for(int i=n;i>=0;i--){
+        printf("%d\n",i);
+    }*/
+    int soma=0;
+    for(int i=0;i<=200;i=i+7){
+        if(i>=100){
+            soma = soma + i;
+        }
+        }
+
+}
